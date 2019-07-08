e053c2334e0e87dae4b8d6dc9f86bc64fe69d0ca
Yliess <hatiyliess86@gmail.com>
2019-07-08 11:07:21 +0200

    Add all required missing libraries

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+apt install libgconf2.0
+apt install libnss3
 apt install libgtk2.0
+apt install libasound2
 apt install xvfb
 
-conda install -c plotly plotly-orca
+conda install -c plotly plotly-orca poppler
 python3 config.py
